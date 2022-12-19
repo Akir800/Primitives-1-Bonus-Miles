29bdee786169a9f9352a9e4919876306094ff1df
Alex Kir <alexkir800@gmail.com>
2022-12-19 23:35:45 +0200

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 5600;
+        int bonusMiles = 20;
+
+        int bonus = price / bonusMiles;
+
+        System.out.println("Alex, How are you? Your bonus" + bonus);
+    }
+}
